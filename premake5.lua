3d5352e9f1a8aa74c0000e047a3151f1303fc4a0
anzz1 <anzz1@live.com>
2023-10-18 19:42:05 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,45 @@
 workspace "openspy-client"
-   configurations { "Release" }
-   platforms { "Win32", "x64" }
-   location "build"
-   objdir ("build/obj")
-   buildlog ("build/log/%{prj.name}.log")
+  configurations { "Release" }
+  platforms { "Win32", "x64" }
+  location "build"
+  objdir ("build/obj")
+  buildlog ("build/log/%{prj.name}.log")
 
-   characterset ("MBCS")
-   staticruntime "Off"
-   exceptionhandling "Off"
-   floatingpoint "Fast"
-   floatingpointexceptions "Off"
-   intrinsics "On"
-   rtti "Off"
-   flags { "NoBufferSecurityCheck", "NoIncrementalLink", "NoManifest", "NoPCH", "NoRuntimeChecks", "OmitDefaultLibrary" }
-   buildoptions { "/kernel", "/Gs1000000" }
-   linkoptions { "/kernel", "/SAFESEH:NO", "/GUARD:NO", "/EMITPOGOPHASEINFO", "/RELEASE", "/DEBUG:NONE", "/NOIMPLIB", "/NOEXP", "/IGNORE:4104" }
+  characterset ("ASCII")
+  staticruntime "Off"
+  exceptionhandling "Off"
+  floatingpoint "Fast"
+  floatingpointexceptions "Off"
+  intrinsics "On"
+  rtti "Off"
+  flags { "NoBufferSecurityCheck", "NoIncrementalLink", "NoManifest", "NoPCH", "NoRuntimeChecks", "OmitDefaultLibrary" }
+  buildoptions { "/kernel", "/Gs1000000" }
+  linkoptions { "/kernel", "/SAFESEH:NO", "/GUARD:NO", "/EMITPOGOPHASEINFO", "/RELEASE", "/DEBUG:NONE", "/NOIMPLIB", "/NOEXP", "/IGNORE:4104" }
 
-   filter "configurations:Release"
-      runtime "Release"
-      defines "NDEBUG"
-      optimize "Speed"
-      symbols "Off"
+  filter "configurations:Release"
+    runtime "Release"
+    defines "NDEBUG"
+    optimize "Speed"
+    symbols "Off"
 
-   filter "platforms:Win32"
-      architecture "x86"
+  filter "platforms:Win32"
+    architecture "x86"
 
-   filter "platforms:x64"
-      architecture "x64"
-      linkoptions { "/HIGHENTROPYVA:NO" }
+  filter "platforms:x64"
+    architecture "x64"
+    linkoptions { "/HIGHENTROPYVA:NO" }
 
 project "openspy-client"
-   kind "SharedLib"
-   language "C"
-   targetextension ".dll"
-   targetdir "bin"
-   files { "dllmain.c", "include/*.h", "iathook/iathook.h" }
-   entrypoint "DllMain"
-   filter "platforms:Win32"
-      targetname "openspy.x86"
-   filter "platforms:x64"
-      targetname "openspy.x64"
+  kind "SharedLib"
+  language "C"
+  targetextension ".dll"
+  targetdir "bin"
+  files { "dllmain.c", "include/*.h", "iathook/iathook.h" }
+  entrypoint "DllMain"
+  filter "platforms:Win32"
+    targetname "openspy.x86"
+  filter "platforms:x64"
+    targetname "openspy.x64"
 
 if _ACTION and _ACTION >= "vs2010" then
   require "vstudio"
